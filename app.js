9aeecf03093be332243c83508df2fdd7aa0d90d8
Duane Wellington <duanewellington@Duanes-MacBook-Pro.local>
2023-10-31 00:36:58 -0400

    improved gameboard

--- app.js
+++ app.js
@@ -14,25 +14,62 @@ const gameOver = false;
 window.onload = function() {
    document.getElementById('minesCount').innerText = minesCount; 
 
-
-
 // auto-generate the board
 for (let r = 0; r < rows; r++) {
     let row = [];
     for (let c = 0; c < columns; c++) {
         let tile = document.createElement('div');
-        tile.id = r.toString() + '-' + c.toString();
-        tile.addEventListener('click', lClickTile);
-        tile.addEventListener('contextmenu', rClickTile => {
-            rClickTile.preventDefault();
-        });
+        tile.id = r.toString() + "-" + c.toString();
         document.getElementById('board').append(tile);
         row.push(tile);
-    }
+        }
     board.push(row);
 }
 console.log(board);
-} 
+}
+
+// randomly place mines on the board
+
+const field = document.getElementById('minesCount');
+for (let i=1;i <= minesCount; i++) {
+    let place = document.createElement('div');
+    place.className = 'place';
+    field.appendChild(place);
+
+    const child = document.createElement('div');
+    child.className = 'child';
+    field.appendChild(child);
+}
+
+// searching for mines nearby, and adding numbers to tiles
+function checkMine(r, c) {
+    if (c < 0 || c >= columns || r , 0 || r >= rows) {
+        return;
+    }
+    let minesFound = 0;
+
+    minesFound += checkTile(r-1, c-1);  // top left
+    minesFound += checkTile(r-1, c);    // top
+    minesFound += checkTile(r-1, c+1);  // top right
+
+    minesFound += checkTile(r, c-1);    // left
+    minesFound += checkTile(r, c+1);    // right
+
+    minesFound += checkTile(r+1, c-1);  // bottom left
+    minesFound += checkTile(r+1, c);    //bottom
+    minesFound += checkTile(r+1, c+1);  // bottom right
+    
+    if (minesFound > 0) {
+        board[r][c].innerText = minesFound;
+        board[r][c].classList.add('clr' + minesFound.toString());
+    }
+}
+
+        // let rTile = document.createElement('div');
+
+                // rTile.addEventListener('contextmenu', rClickTile => {
+            // rClickTile.preventDefault();
+
 
 // function rClickTile() {
 //     let tile = this;
@@ -53,5 +90,3 @@ console.log(board);
 //     if (flagEnabled === true) {
 // return
 //     } else (
-//   )
-// }
