b4017bafdce456f9dc42b95f20336e5943371b46
Duane Wellington <duanewellington@Duanes-MacBook-Pro.local>
2023-10-31 16:35:03 -0400

    adjustments

--- sandbox/app.js
+++ sandbox/app.js
@@ -12,8 +12,23 @@ const flagEnabled = false;
 const gameOver = false;
 
 window.onload = function () {
+  startGame();
+}
+
+// check -> place mines on the board
+function setMines() {
+  minesLocation.push('2-2');
+  minesLocation.push('2-3');
+  minesLocation.push('5-6');
+  minesLocation.push('3-4');
+  minesLocation.push('1-1');
+}
+
+
+function startGame() {
   //-- ICEBOX -> get minesDisplay to read as 3 digits at all times{
   document.getElementById("minesOnBoard").textContent = "00" + minesCount;
+  setMines();
 
   // auto-generate the board
   for (let r = 0; r < rows; r++) {
@@ -28,11 +43,6 @@ window.onload = function () {
   }
   console.log(board);
 
-  
-  // place mines on the board
-
-
-  
   // add Event Listener to clear tiles using left mouse button
   document.addEventListener("click", lButton);
   function lButton() {
@@ -48,19 +58,20 @@ window.onload = function () {
 
 // randomly place mines on the board
 
-function placeMines(board, minesCount) {
-  let minesPlaced = 0;
-  while (minesPlaced < minesCount) {
-    const row = Math.floor(Mth.random() * rows);
-    const column = Math.floor(Math.random() * columns);
+// function placeMines(board, minesCount) {
+//   let minesPlaced = 0;
+//   while (minesPlaced < minesCount) {
+//     const row = Math.floor(Mth.random() * rows);
+//     const column = Math.floor(Math.random() * columns);
 
-    if (board[row][column] !== -1) {
-      board[row][column] = -1;
-      minesPlaced++;
-    }
-  }
-}
+//     if (board[row][column] !== -1) {
+//       board[row][column] = -1;
+//       minesPlaced++;
+//     }
+//   }
+// }
 
+// get coordinates for mines that will be placed on the board
 function getMineCoordinates(board, x) {
   const mineCoordinates = [];
   for (let row = 0; row < rows; row++) {
@@ -74,8 +85,10 @@ function getMineCoordinates(board, x) {
     }
   }
   return mineCoordinates;
-  console.log(mineCoordinates)
+  console.log(getMineCoordinates)
 }
+
+
 const mineField = document.getElementById("minesOnBoard");
 for (let i = 1; i <= minesCount; i++) {
   let place = document.createElement("div");
@@ -106,14 +119,57 @@ let timer = setInterval(function () {
   }
 }, 1000);
 
-// cover mines?
+
+function rClickTile() {
+
+  let tile = this;
+  if (flagEnabled == false) {
+      if (tile.innerText == "") {
+          tile.innerText = 'F'
+      } else if (flagEnabled == true) {
+          if (tile.innertext = 'F'); {
+              tile.innerText = "";
+  }
+  } return;
+}
+
+if (minesLocation.includes(tile.id)) {
+  gameOver = true;
+  return;
+}
+
+let coords = tile.id.split('-')
+let r = parseInt(coords[0]);
+let c = parseInt(coords[1]);
+checkMine(r, c)
+;}
 
 //
 // searching for mines nearby, and adding numbers to tiles
+
+
+function showMines() {
+  for (let r=0; r , rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = board[r][c];
+      if (minesLocation.includes(tile.id)) {
+        tile.innerText = 'B';
+        tile.style.backgroundColor = 'red'
+      }
+    }
+  }
+}
+
+// recursion flood event
 function checkMine(r, c) {
   if ((c < 0 || c >= columns || r, 0 || r >= rows)) {
     return;
   }
+  if (board[r][c].classList.contains('tile-clicked')) {
+    return;
+  }
+board[r][c].classList.add('tile-clicked')
+
   let minesFound = 0;
 
   minesFound += checkTile(r - 1, c - 1); // top left
@@ -131,8 +187,34 @@ function checkMine(r, c) {
     board[r][c].innerText = minesFound;
     board[r][c].classList.add("clr" + minesFound.toString());
   }
+  else {
+    checkMine(r - 1, c - 1);  // top left
+    checkMine(r - 1, c);      // top
+    checkMine(r - 1, c + 1);  // top right
+
+    checkMine(r, c - 1);  // left
+    checkMine(r, c + 1);  // right
+
+    checkMine(r + 1, c - 1);  // bottom left
+    checkMine(r + 1, c);  // bottom
+    checkMine(r + 1, c -+1);  // bottom right
+  }
+
+  if (tilesClicked == rows * columns - minesCount) {
+    document.getElementById('minesOnBoard').innerText = '000'
+  }
 }
 
+
+
+function checkTile(r, c) {
+  if (r < 0 || r >= rows || c >= columns) {
+    return 0;
+  }
+  if (minesLocation.includes(r.toString() + '-' + c.toString())) {
+    return 1;
+  }
+}
 /* gameOver function (Smiley turns to exploded head; timer stops; console.log("You WIN!!") with minefield fully exposed showing all mines, or ("You LOSE!!" with minefield fully exposed and the triggered mine tile in red, etc) and option to restart!! */
 
 // let rTile = document.createElement('div');
@@ -140,15 +222,4 @@ function checkMine(r, c) {
 // rTile.addEventListener('contextmenu', rClickTile => {
 // rClickTile.preventDefault();
 
-// function rClickTile() {
-//     let tile = this;
-//     if (flagEnabled === false) {
-//         if (tile.innerText == "") {
-//             tile.innerText = '&#128681'
-//         } else if (flagEnabled === true) {
-//             if (tile.innertext = '&#128681'); {
-//                 tile.innerText = "";
-//     }
-//     }
-// }
 // }
